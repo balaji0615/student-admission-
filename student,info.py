adc81247a5a2d5f78ee20c60450089dd100b0553
TCS <mohamedrayyanjameel@gmail.com>
2022-11-09 13:26:50 +0530

    new

--- student,info.py
+++ student,info.py
@@ -1,6 +1,7 @@
-a=input("ENTER STUDENT NAME=");
-b=int(input("ENTER STUDENT ROOL NO=");
-c=input("ENTER YOUR ADHARE DETAILS=");
+a=input("ENTER STUDENT NAME=")
+b=int(input("ENTER STUDENT ROOL NO=")
+c=input("ENTER YOUR ADHARE DETAILS=")
 print(a)
 print(b)
 print(c)
+print("over")
